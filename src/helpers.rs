ad2aef6548aec7ea33da6eda69ffe436f81075b4
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-07-09 13:08:16 +0300

    Adding tests
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,9 @@
+pub fn is_valid_temperature_scale(temperature_scale: &str) -> bool {
+    return temperature_scale == "F" || temperature_scale == "C";
+}
+
 pub fn convert(temperature: &f32, temperature_scale: &str) -> f32 {
-    if temperature_scale != "F" && temperature_scale != "C" {
+    if !is_valid_temperature_scale(temperature_scale) {
         panic!("Expected value 'F' or 'C'");
     }
 
@@ -10,6 +14,29 @@ pub fn convert(temperature: &f32, temperature_scale: &str) -> f32 {
     return (temperature - 32 as f32) * 0.5556;
 }
 
-pub fn is_valid_temperature_scale(temperature_scale: &str) -> bool {
-    return temperature_scale == "F" || temperature_scale == "C";
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_valid_temperature_scale_test() {
+        assert_eq!(is_valid_temperature_scale(&"F"), true);
+        assert_eq!(is_valid_temperature_scale(&"C"), true);
+        assert_eq!(is_valid_temperature_scale(&"a"), false);
+    }
+
+    #[test]
+    fn convert_test() {
+        // To Fahrenheit
+        assert_eq!(convert(&(0 as f32), &"F"), 32);
+        assert_eq!(convert(&(2 as f32), &"F"), 35.6);
+        assert_eq!(convert(&(5 as f32), &"F"), 41);
+        assert_eq!(convert(&(23 as f32), &"F"), 73.4);
+
+        // To Celsius
+        assert_eq!(convert(&(0 as f32), &"C"), -17.7778);
+        assert_eq!(convert(&(2 as f32), &"C"), -16.6667);
+        assert_eq!(convert(&(5 as f32), &"C"), -15);
+        assert_eq!(convert(&(23 as f32), &"C"), -5);
+    }
 }
